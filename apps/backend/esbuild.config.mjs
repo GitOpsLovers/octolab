ba8510edb04ec008cbbea66fe93f51e9ba985b68
Marc Fernandez <mcfdez87@gmail.com>
2025-07-15 19:30:55 +0200

    chore: add start to backend

--- apps/backend/esbuild.config.mjs
+++ apps/backend/esbuild.config.mjs
@@ -1,12 +1,20 @@
 import { build } from "esbuild";
+import { resolve } from "path";
+
+const projectRoot = resolve(process.cwd(), "src");
 
 build({
   entryPoints: ["src/index.ts"],
   outfile: "dist/index.js",
   platform: "node",
   minify: false,
-  bundle: false,
+  bundle: true,
   sourcemap: true,
   treeShaking: true,
   format: "cjs",
+  tsconfig: "tsconfig.json",
+  alias: {
+    "@core": resolve(projectRoot, "core"),
+    "@features": resolve(projectRoot, "features"),
+  },
 }).catch(() => process.exit(1));
