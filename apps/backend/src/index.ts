1844447a4a52c850fc98b3e6c03dfdfef7d441e4
Marc Fernandez <mcfdez87@gmail.com>
2025-07-15 20:25:01 +0200

    chore: add turbo to backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,19 +1,16 @@
 // eslint-disable-next-line import/no-unassigned-import
-/* import 'dotenv/config'; */
+import 'dotenv/config';
 
 import bodyParser from 'body-parser';
 import express from 'express';
 import helmet from 'helmet';
 
-/* import { initSupabaseClient } from '@core/infrastructure/database/supabase.client'; */
+import { initSupabaseClient } from '@core/infrastructure/database/supabase.client';
 import { configureCorsMiddleware } from '@core/infrastructure/express/cors-config.express';
 import { checkRequiredEnvVariables } from '@core/infrastructure/express/env-config.express';
-/* import { templatesRoutes } from '@features/templates/ui/routes/templates.routes';
+import { templatesRoutes } from '@features/templates/ui/routes/templates.routes';
 import { usersRoutes } from '@features/users/ui/routes/users.routes';
-import { workflowsRoutes } from '@features/workflows/ui/routes/workflows.routes'; */
-
-console.log("AUTH0_DOMAIN in runtime:", process.env.AUTH0_DOMAIN);
-console.log("AUTH0_AUDIENCE in runtime:", process.env.AUTH0_AUDIENCE);
+import { workflowsRoutes } from '@features/workflows/ui/routes/workflows.routes';
 
 const requiredEnvVars = [
     'BACKEND_PORT',
@@ -30,7 +27,7 @@ const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS?.split(',') ?? [];
 
 checkRequiredEnvVariables(requiredEnvVars);
 
-/* initSupabaseClient(); */
+initSupabaseClient();
 
 const app = express();
 
@@ -43,9 +40,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(configureCorsMiddleware(allowedOrigins));
 
 // Routing
-/* app.use('/api', usersRoutes);
+app.use('/api', usersRoutes);
 app.use('/api', templatesRoutes);
-app.use('/api', workflowsRoutes); */
+app.use('/api', workflowsRoutes);
 
 const PORT = process.env.BACKEND_PORT || 4000;
 
