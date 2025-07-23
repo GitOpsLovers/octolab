de8017a830499d316923f6dcc95f408b73c2e282
Marc Fernandez <mcfdez87@gmail.com>
2025-07-23 08:13:05 +0200

    feat: add banner to show beta phase

--- apps/frontend/src/app/(with-header)/layout.tsx
+++ apps/frontend/src/app/(with-header)/layout.tsx
@@ -1,6 +1,7 @@
 import { CookiesProvider } from 'next-client-cookies/server';
 import { ReactNode } from 'react';
 
+import { BetaBanner } from '@ui/layout/components/beta-banner.component';
 import { Header } from '@ui/layout/components/header.component';
 import { AuthUserProvider } from '@ui/user/providers/auth-user.provider';
 
@@ -11,7 +12,9 @@ export default function WithHeaderLayout({ children }: { children: ReactNode }) 
     return (
         <AuthUserProvider>
             <CookiesProvider>
+                <BetaBanner />
                 <Header />
+
                 {children}
             </CookiesProvider>
         </AuthUserProvider>
