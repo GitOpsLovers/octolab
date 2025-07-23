ea7f9ea476ce8a65b3489ef9d7297b6a8c7ced7d
Marc Fernandez <mcfdez87@gmail.com>
2025-07-23 08:44:32 +0200

    feat: add Auth0 actions middleware (#35)

--- apps/backend/src/features/users/ui/routes/users.routes.ts
+++ apps/backend/src/features/users/ui/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import { createUserController } from '../controllers/create-user.controller';
 import { getCurrentUserController } from '../controllers/get-current-user.controller';
 
+import { auth0ActionMiddleware } from '@core/ui/middlewares/auth0-actions.middleware';
 import { authClient } from '@features/authentication/infrastructure/auth0/authentication.client';
 
 const usersRoutes = Router();
@@ -10,5 +12,6 @@ const usersRoutes = Router();
  * Users routes
  */
 usersRoutes.get('/current-user', authClient, getCurrentUserController);
+usersRoutes.post('/user', auth0ActionMiddleware, createUserController);
 
 export { usersRoutes };
