1328c62807ed1c8c37117d39bf47a6580806a359
Marc Fernandez <mcfdez87@gmail.com>
2025-07-30 15:12:29 +0200

    feat: add access to templates output

--- apps/backend/src/features/authentication/application/send-welcome-email.use-case.ts
+++ apps/backend/src/features/authentication/application/send-welcome-email.use-case.ts
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-nodejs-modules */
+import { readFileSync } from 'fs';
+import path from 'path';
+
 import { WelcomeEmailDto } from '../domain/dtos/welcome-email.dto';
 
 import { EmailClient } from '@core/domain/interfaces/email-client.interface';
@@ -9,6 +13,19 @@ import { EmailClient } from '@core/domain/interfaces/email-client.interface';
  * @param welcomeData Welcome email data.
  */
 export async function sendWelcomeEmailUseCase(emailClient: EmailClient, welcomeData: WelcomeEmailDto): Promise<void> {
+    const templatePath = path.resolve(__dirname, `${process.env.EMAIL_TEMPLATES_PATH}/welcome-email.html`);
+    console.log(templatePath);
+
+    let htmlContent2: string;
+
+    try {
+        htmlContent2 = readFileSync(templatePath, 'utf8');
+        console.log(htmlContent2);
+    } catch (err) {
+        console.error('❌ Error reading email template:', err);
+        return;
+    }
+
     const htmlContent = `
         <h2>New Template Proposal</h2>
     `;
