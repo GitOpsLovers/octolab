24851c1eb283130df71175867ee85309aea7a719
Marc Fernandez <mcfdez87@gmail.com>
2025-07-30 15:43:01 +0200

    feat: add Auth0 middleware to Welcome email

--- apps/backend/src/features/authentication/ui/routes/authentication.routes.ts
+++ apps/backend/src/features/authentication/ui/routes/authentication.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { sendWelcomeEmailController } from '../controllers/send-welcome-email.controller';
 
+import { auth0ActionMiddleware } from '@core/ui/middlewares/auth0-actions.middleware';
+
 const authRoutes = Router();
 
 /**
  * Authentication routes
  */
-authRoutes.post('/send-welcome-email', sendWelcomeEmailController);
+authRoutes.post('/send-welcome-email', auth0ActionMiddleware, sendWelcomeEmailController);
 
 export { authRoutes };
