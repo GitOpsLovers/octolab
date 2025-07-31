fcab39df2d37bd1c276cf57ad5285b119b040b96
Marc Fernandez <mcfdez87@gmail.com>
2025-07-31 08:39:46 +0200

    feat: modify toasts to use theme colors

--- apps/frontend/src/ui/layout/configs/toaster.config.ts
+++ apps/frontend/src/ui/layout/configs/toaster.config.ts
@@ -13,29 +13,29 @@ export const toasterConfig = {
     },
     success: {
         style: {
-            background: 'var(--surface)',
-            color: 'var(--color-success)',
-            border: '1px solid var(--color-success)',
+            background: 'var(--primary)',
+            color: 'var(--color-white)',
+            border: '1px solid var(--color-primary)',
             fontSize: '1rem',
             padding: '1rem 1.5rem',
             minWidth: '280px',
         },
         iconTheme: {
-            primary: 'var(--color-success)',
+            primary: 'var(--color-white)',
             secondary: 'var(--surface)',
         },
     },
     error: {
         style: {
-            background: 'var(--surface)',
-            color: 'var(--color-danger)',
+            background: 'var(--danger)',
+            color: 'var(--color-white)',
             border: '1px solid var(--color-danger)',
             fontSize: '1rem',
             padding: '1rem 1.5rem',
             minWidth: '280px',
         },
         iconTheme: {
-            primary: 'var(--color-danger)',
+            primary: 'var(--color-white)',
             secondary: 'var(--surface)',
         },
     },
